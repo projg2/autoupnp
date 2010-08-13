3864c2cb65a9af9d7bedfe4fcfcfd5f46f76a00f
Michał Górny <gentoo@mgorny.alt.pl>
2010-08-13 18:26:14 +0200

    Introduce simple transparent wrapper for libc funcs.

--- autoupnp.c
+++ autoupnp.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 
+#include <sys/socket.h>
+#include <unistd.h>
+
 enum replaced_func {
 	rf_bind = 0,
 	rf_listen,
@@ -32,4 +35,24 @@ static void* const get_func(const enum replaced_func rf) {
 	}
 
 	return funcs[rf];
+}
+
+int bind(const int socket, const struct sockaddr* const address,
+		const socklen_t address_len) {
+	const int (*bind_func)(int, const struct sockaddr*, socklen_t) =
+		get_func(rf_bind);
+
+	return bind_func(socket, address, address_len);
+}
+
+int listen(const int socket, const int backlog) {
+	const int (*listen_func)(int, int) = get_func(rf_listen);
+
+	return listen_func(socket, backlog);
+}
+
+int close(const int fildes) {
+	const int (*close_func)(int) = get_func(rf_close);
+
+	return close_func(fildes);
 }
