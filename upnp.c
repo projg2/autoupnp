29df7ea3753ebfba8ceedbcc7e5fbf766a6533ef
Michał Górny <gentoo@mgorny.alt.pl>
2010-08-13 23:54:11 +0200

    Actual port forwarding support.

--- upnp.c
+++ upnp.c
@@ -8,6 +8,8 @@
 #include <syslog.h>
 
 #include <miniupnpc/miniupnpc.h>
+#include <miniupnpc/upnpcommands.h>
+#include <miniupnpc/upnperrors.h>
 
 #include "upnp.h"
 
@@ -15,7 +17,7 @@ static const int discovery_delay = 2000; /* [ms] */
 
 struct igd_data {
 	struct UPNPUrls urls;
-	struct IGDdatas datas;
+	struct IGDdatas data;
 
 	char lan_addr[16];
 };
@@ -28,12 +30,11 @@ static struct igd_data* setup_igd(void) {
 	if (!igd_set_up) {
 		struct UPNPDev* devlist = upnpDiscover(discovery_delay, NULL, NULL, 0);
 
-		if (UPNP_GetValidIGD(devlist, &(igd_data.urls), &(igd_data.datas),
-					igd_data.lan_addr, sizeof(igd_data.lan_addr))) {
+		if (UPNP_GetValidIGD(devlist, &(igd_data.urls), &(igd_data.data),
+					igd_data.lan_addr, sizeof(igd_data.lan_addr)))
 			igd_set_up = 1;
-
-			syslog(LOG_INFO, "IGD found, local IP: %s!", igd_data.lan_addr);
-		}
+		else
+			syslog(LOG_ERR, "(AutoUPNP) Unable to find an IGD on the network.");
 
 		freeUPNPDevlist(devlist);
 	}
@@ -51,10 +52,39 @@ static void dispose_igd(void) {
 }
 
 void enable_redirect(struct registered_socket_data* rs) {
-	setup_igd();
-	syslog(LOG_INFO, "(AutoUPNP) Enabling the redirect for port %s", rs->port);
+	const struct igd_data* igd_data = setup_igd();
+
+	if (igd_data) {
+		const int ret = UPNP_AddPortMapping(
+				igd_data->urls.controlURL,
+				igd_data->data.servicetype,
+				rs->port, rs->port, igd_data->lan_addr,
+				"AutoUPNP-added port forwarding",
+				rs->protocol, NULL);
+
+		if (ret == 0)
+			syslog(LOG_INFO, "(AutoUPNP) Port %s/%s forwarded successfully to %s.",
+					rs->port, rs->protocol, igd_data->lan_addr);
+		else
+			syslog(LOG_ERR, "(AutoUPNP) UPNP_AddPortMapping(%s, %s, %s) failed: %d (%s).",
+					rs->port, igd_data->lan_addr, rs->protocol, ret, strupnperror(ret));
+	}
 }
 
 void disable_redirect(struct registered_socket_data* rs) {
-	syslog(LOG_INFO, "(AutoUPNP) Disabling the redirect for port %s", rs->port);
+	const struct igd_data* igd_data = setup_igd();
+
+	if (igd_data) {
+		const int ret = UPNP_DeletePortMapping(
+				igd_data->urls.controlURL,
+				igd_data->data.servicetype,
+				rs->port, rs->protocol, NULL);
+
+		if (ret == 0)
+			syslog(LOG_INFO, "(AutoUPNP) Port forwarding for port %s/%s removed successfully.",
+					rs->port, rs->protocol);
+		else
+			syslog(LOG_ERR, "(AutoUPNP) UPNP_DeletePortMapping(%s, %s) failed: %d (%s).",
+					rs->port, rs->protocol, ret, strupnperror(ret));
+	}
 }
